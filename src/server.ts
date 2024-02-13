57b22e822b6bddbf438591bb706c336cf820a929
Jérôme Meichelbeck <jerome@meichelbeck.io>
2024-02-13 21:09:18 +0100

    Add api prefix to main router

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(mainRouter);
+app.use("/api", mainRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
