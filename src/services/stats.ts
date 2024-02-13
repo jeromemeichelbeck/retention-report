2d535911cca06ea3d97f860b99dbcdf8f2cf2b83
Jérôme Meichelbeck <jerome@meichelbeck.io>
2024-02-13 23:35:35 +0100

    [WIP} Request for first period

--- src/services/stats.ts
+++ src/services/stats.ts
@@ -1,3 +1,4 @@
+import { db } from "../db/db";
 import { Month } from "../types/Month";
 import { generatePeriods, getThisMonth, isPeriodInvalid } from "../utils/dates";
 
@@ -9,6 +10,42 @@ export const getClientsRetention = async (referenceMonth: Month) => {
   }
 
   const retentionReport = generatePeriods(referenceMonth, thisMonth);
+
+  const firstPeriod = retentionReport[0];
+
+  if (!firstPeriod) {
+    return retentionReport;
+  }
+
+  console.log(`'${firstPeriod.month}%'`);
+
+  const statement = db.prepare(`
+    SELECT
+      DISTINCT a.client_id,
+      e.employee_id,
+      e.first_name,
+      e.last_name
+    FROM
+      APPOINTMENTS a
+    LEFT JOIN
+      EMPLOYEES e
+    ON
+      a.employee_id = e.employee_id
+    WHERE
+      a.date LIKE ?;
+    ORDER BY
+      a.date ASC; 
+    `);
+  const appointments = statement
+    .bind(`${firstPeriod.month}%`)
+    .all((err, rows) => {
+      if (err) {
+        throw new Error("Error while fetching appointments");
+      }
+      console.log(rows);
+    });
+
+  // console.log(appointments);
 
   return retentionReport;
 };
