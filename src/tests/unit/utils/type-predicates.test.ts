37bfeaef9ee7b535f6e3dbe75d15f2037578b218
Jérôme Meichelbeck <jerome@meichelbeck.io>
2024-02-13 22:26:38 +0100

    Add unit test

---
+++ src/tests/unit/utils/type-predicates.test.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect } from "vitest";
+import { isMonth } from "../../../utils/type-predicates";
+
+describe("Type Predicates", () => {
+  it("should return false if random string", () => {
+    expect(isMonth("test")).toBe(false);
+  });
+
+  it("should return false not a valid date", () => {
+    expect(isMonth("2023-42")).toBe(false);
+  });
+
+  it("should return true for valid date", () => {
+    expect(isMonth("2023-07")).toBe(true);
+  });
+});
