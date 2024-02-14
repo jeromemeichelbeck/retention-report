e800463c4e1e007436592e7e4755e7f403f604d6
Jérôme Meichelbeck <jerome.meichelbeck@bionext.lu>
2024-02-14 08:46:35 +0100

    Handle server startup errors

--- src/server.ts
+++ src/server.ts
@@ -13,6 +13,11 @@ app.use("/api", mainRouter);
 
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+app
+  .listen(3000, () => {
+    console.log("Server is running on port 3000");
+  })
+  .on("error", (err) => {
+    console.error("Error starting server:", err.message);
+    process.exit(1);
+  });
